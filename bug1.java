ad565616a5ac824279c5aa0aebefdc5fc7f5fb56
SophieYing <yingchen0605@gmail.com>
2018-05-29 23:46:04 -0700

    modify some text11:46

---
+++ bug1.java
@@ -0,0 +1,3 @@
+public void modifybug(){
+	System.out.println();
+}
--- try_two.java
+++ try_two.java
@@ -1,7 +1,7 @@
 public static void main(String[] args){
 	System.out.println("master dev modify");
 	System.out.println("Hello --world!");
-	System.out.println();
+	System.out.println("modify bug!!!!!");
 	//bug fix
 	System.out.println("bug fix");
 }
