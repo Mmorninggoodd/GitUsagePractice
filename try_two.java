6028acaf10e16555e34ef42935eefd703e82da21
SophieYing <yingchen0605@gmail.com>
2018-05-29 23:23:45 -0700

    first commit

---
+++ try_two.java
@@ -0,0 +1,7 @@
+public static void main(String[] args){
+	System.out.println("master dev modify");
+	System.out.println("Hello --world!");
+	System.out.println();
+	//bug fix
+	System.out.println("bug fix");
+}
