65aafe32194bb28a822c7961485fd2c1b9a316b7
SophieYing <yingchen0605@gmail.com>
2018-05-30 00:06:49 -0700

    Add another print from local after add the-comment-after-another-branch

--- try_two.java
+++ try_two.java
@@ -5,5 +5,10 @@ public static void main(String[] args){
 	//bug fix
 	System.out.println("bug fix");
 	System.out.println("Add another one branch!");
+<<<<<<< HEAD
 	System.out.println("Add after another branch already added!");
 }
+=======
+	System.out.println("Add another print from local after add the-comment-after-another-branch");
+}
+>>>>>>> Add another print from local after add the-comment-after-another-branch
